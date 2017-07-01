cd349eab0a3a366313a21b40d7d5ff5920e77fce
j95alvarez <juanalvarez391@yahoo.com>
2017-06-30 18:38:34 -0700

    enemies

---
+++ New Unity Project/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    public int speed;
+    public int hp;
+    public GameObject playerBase;
+
+	// Use this for initialization
+	void Start () {
+        playerBase = GameObject.FindGameObjectWithTag("Base");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerBase != null) {
+            transform.position = Vector3.MoveTowards(transform.position, playerBase.transform.position, speed * Time.deltaTime);
+        }
+
+	}
+}
