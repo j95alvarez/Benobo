eb4b3d6914e4f0cf67cc652cbed60bd965e1019d
jrman411 <junioralvarez411@gmail.com>
2017-06-30 21:09:45 -0700

    Player Movement
    movement using the “WASD” keys

---
+++ New Unity Project/Assets/PlayreMov.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayreMov : MonoBehaviour {
+	public int speed;
+	public int hp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (0, speed * Time.deltaTime, 0);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (-speed * Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (0, -speed * Time.deltaTime, 0);
+		}
+		if(Input.GetKey(KeyCode.D)) {
+			transform.Translate (speed * Time.deltaTime, 0, 0);
+		}
+	}
+}
